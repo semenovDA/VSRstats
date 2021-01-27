90464164e57a7967c3cce1db2bf1a05ed76f6337
Dmitry <medtex03@gmail.com>
2021-01-27 12:41:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 
 setup(
     name='VSRstats',
-    version='1.13',
+    version='1.14',
     author='Dmitry',
     author_email='semenov.da17@gmail.com',
     packages=['VSRstats'],
